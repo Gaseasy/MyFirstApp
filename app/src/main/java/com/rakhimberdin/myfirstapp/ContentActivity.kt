aa18beea4749dd1ceb3af07da71964c8845eab84
Rakhimberdin Yan <itshkola@rambler.ru>
2023-03-02 23:10:40 +0200

    Лабораторная 4: работа с фрагментами.

--- app/src/main/java/com/rakhimberdin/myfirstapp/ContentActivity.kt
+++ app/src/main/java/com/rakhimberdin/myfirstapp/ContentActivity.kt
@@ -2,12 +2,24 @@ package com.rakhimberdin.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ContentActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content)
 
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav);
 
+        bottomNav.setupWithNavController(navController);
     }
 }
